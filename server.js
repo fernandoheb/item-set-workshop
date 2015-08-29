d05fcb3ce8255d17bcf9a27c2d71d90dcf04dbbf
josed92 <jose_daniel_d@hotmail.com>
2015-08-29 13:02:11 -0430

    se puede pedir un diccionario que mapea chaps ids a los nombres de las imagenes y se puede pedir las versiones de ddragon por region

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/app/' + 'index.html');
 });
 
+/*
+//daily update
+function daily(){
+  return scripts.daily();
+}
+daily();*/
+
 // Give Match History
 app.get(/^\/userName=%22(.+)%22&region=%22(.+)%22$/, function(req, res){
   if(!scripts.inArray(lolServers,req.params[1])){
@@ -37,6 +44,23 @@ app.get(/^\/matchId=%22(.+)%22&region=%22(.+)%22$/, function(req, res){
     res.send(text);
   });
 });
+
+//Give ddragonVersion
+app.get(/^\/ddragon&region=%22(.+)%22$/, function(req, res){
+  if(!scripts.inArray(lolServers,req.params[0])){
+    res.send('{"error":"invalid server"}');
+    return;
+  }
+  var i = 0;
+  while(lolServers[i]!=req.params[0]){i++;}
+  return res.send(scripts.ddragonVersion[i]);
+});
+
+//Give champions dictionary
+app.get(/^\/champDic$/, function(req, res){
+  return res.send(scripts.champions);
+});
+
 
 // Start server
 app.listen(port, ip);
--- server/scripts.js
+++ server/scripts.js
@@ -1,6 +1,9 @@
 const key='api_key=f39f8547-5363-4eaa-ad2c-f76f5ed9bbd2';
 const http = require('http');
 const https = require("https");
+const lolServers = ["br","lan","eune","euw","kr","las","na","oce","ru","tr"];
+var ddragonVersion = ["5.16.1","5.16.1","5.16.1","5.16.1","5.16.1","5.16.1","5.16.1","5.16.1","5.16.1","5.16.1"];
+var champions = '{"1":"Annie","2":"Olaf","3":"Galio","4":"TwistedFate","5":"XinZhao","6":"Urgot","7":"Leblanc","8":"Vladimir","9":"FiddleSticks","10":"Kayle","11":"MasterYi","12":"Alistar","13":"Ryze","14":"Sion","15":"Sivir","16":"Soraka","17":"Teemo","18":"Tristana","19":"Warwick","20":"Nunu","21":"MissFortune","22":"Ashe","23":"Tryndamere","24":"Jax","25":"Morgana","26":"Zilean","27":"Singed","28":"Evelynn","29":"Twitch","30":"Karthus","31":"Chogath","32":"Amumu","33":"Rammus","34":"Anivia","35":"Shaco","36":"DrMundo","37":"Sona","38":"Kassadin","39":"Irelia","40":"Janna","41":"Gangplank","42":"Corki","43":"Karma","44":"Taric","45":"Veigar","48":"Trundle","50":"Swain","51":"Caitlyn","53":"Blitzcrank","54":"Malphite","55":"Katarina","56":"Nocturne","57":"Maokai","58":"Renekton","59":"JarvanIV","60":"Elise","61":"Orianna","62":"MonkeyKing","63":"Brand","64":"LeeSin","67":"Vayne","68":"Rumble","69":"Cassiopeia","72":"Skarner","74":"Heimerdinger","75":"Nasus","76":"Nidalee","77":"Udyr","78":"Poppy","79":"Gragas","80":"Pantheon","81":"Ezreal","82":"Mordekaiser","83":"Yorick","84":"Akali","85":"Kennen","86":"Garen","89":"Leona","90":"Malzahar","91":"Talon","92":"Riven","96":"KogMaw","98":"Shen","99":"Lux","101":"Xerath","102":"Shyvana","103":"Ahri","104":"Graves","105":"Fizz","106":"Volibear","107":"Rengar","110":"Varus","111":"Nautilus","112":"Viktor","113":"Sejuani","114":"Fiora","115":"Ziggs","117":"Lulu","119":"Draven","120":"Hecarim","121":"Khazix","122":"Darius","126":"Jayce","127":"Lissandra","131":"Diana","133":"Quinn","134":"Syndra","143":"Zyra","150":"Gnar","154":"Zac","157":"Yasuo","161":"Velkoz","201":"Braum","222":"Jinx","223":"TahmKench","236":"Lucian","238":"Zed","245":"Ekko","254":"Vi","266":"Aatrox","267":"Nami","268":"Azir","412":"Thresh","421":"RekSai","429":"Kalista","432":"Bard"}'
 
 get = function(options,onResult){
     var prot = options.port == 443 ? https : http;
@@ -24,6 +27,32 @@ get = function(options,onResult){
     req.end();
 
 };
+
+/*function daily(){
+  //check ddragon server versions
+  var i = 0;
+  var options = {
+    host: 'global.api.pvp.net',
+    port: 443,
+    path: ('/api/lol/static-data/'+lolServers[i]+'/v1.2/versions?'+key),
+    method: 'GET'
+  };
+  var f = function help(code,text){
+    ddragonVersion[i] = JSON.parse(text)[0];
+    i++;
+    if(i==lolServers.length){return;}
+    options = {
+      host: 'global.api.pvp.net',
+      port: 443,
+      path: ('/api/lol/static-data/'+lolServers[i]+'/v1.2/versions?'+key),
+      method: 'GET'
+    };
+    get(options,f);
+  }
+  get(options,f);
+
+  setTimeout(daily,86399000);
+}*/
 
 /*
 userName as string ej: "pipimax"
@@ -148,17 +177,9 @@ function inArray(arr,obj){
 exports.inArray = inArray;
 exports.getHistory = getHistory;
 exports.getMatch = getMatch;
-/*
-getHistory("JoseD92","lan",function(jj){
-  var j = JSON.parse(jj);
-  getMatch(j.games[0].gameId,false,'lan',function (yy){
-    var y = JSON.parse(yy);
-    console.log(getDetails(y));
-  });
-});/*
-getHistory("JoseD922","lan",function(j){
-  console.log(j);
-});*/
+//exports.daily = daily;
+exports.ddragonVersion = ddragonVersion;
+exports.champions = champions;
 
 
 
