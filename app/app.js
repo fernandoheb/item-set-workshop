9041ed1c655bd3be8b84d3d2b6eccc7cb01d23a6
Manuel Pacheco <manuelalejandropm@gmail.com>
2015-08-29 13:40:51 -0430

    base templates and config

--- app/app.js
+++ app/app.js
@@ -1,12 +1,62 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+// Normally each view is declared in its own module
+angular.module('iswApp', [
   'ngRoute',
+  'ngMaterial',
+
+  'iswApp.welcome',
+  'iswApp.sidenav',
+  'iswApp.searchplayer',
+
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+])
+
+// Configure root route
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/'});
+}])
+
+// Config Angular Material Theming
+.config(['$mdThemingProvider', function($mdThemingProvider) {
+  $mdThemingProvider
+    .theme('default')
+    .dark()
+    .primaryPalette('teal')
+    .backgroundPalette('grey', {default: '900'});
+}])
+
+// Define application level controller
+.controller('AppController', ['$scope', '$location', function($scope, $location) {
+  // The sidenav menu structure
+  $scope.menu = [
+    {
+      name: 'Introduction',
+      route: '/',
+      hide: true
+    },
+    {
+      name: 'Search by Player',
+      route: '/searchplayer'
+    },
+    {
+      name: 'Search by Match ID',
+      route: '/searchmatch'
+    },
+    {
+      name: 'About',
+      route: '/about'
+    }
+  ];
+
+  $scope.getTitle = function() {
+    var i, top = $scope.menu.length;
+    for (i=0; i<top; i++) {
+      var item = $scope.menu[i];
+      if (item.route === $location.path()) return item.name;
+    }
+  };
 }]);
